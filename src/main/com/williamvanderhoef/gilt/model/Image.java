2eb1dffc8b02d063241ce78bbe925ce62b2a66cf
William Vanderhoef <wvanderhoef@gilt.com>
2012-02-02 12:13:30 -0500

    Fixing ignored stuff updating to latest changes.

---
+++ src/main/com/williamvanderhoef/gilt/model/Image.java
@@ -0,0 +1,51 @@
+package com.williamvanderhoef.gilt.model;
+
+/**
+ *
+ * @author wvanderhoef
+ *
+ */
+public class Image {
+
+	private Number height;
+	private Number width;
+	private String url;
+	/**
+	 * @return the height
+	 */
+	public Number getHeight() {
+		return height;
+	}
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(Number height) {
+		this.height = height;
+	}
+	/**
+	 * @return the width
+	 */
+	public Number getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(Number width) {
+		this.width = width;
+	}
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+}
